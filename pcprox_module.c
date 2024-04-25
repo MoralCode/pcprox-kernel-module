24ff93d0e9e144ef8bf5ab0ef09cd37d27062d89
Adrian Edwards <adrian@adriancedwards.com>
2024-04-25 18:44:36 -0400

    convert to hello world module https://sysprog21.github.io/lkmpg/#the-simplest-module

--- pcprox_module.c
+++ pcprox_module.c
@@ -1,49 +1,39 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/usb.h>
+/* 
 
-static struct usb_device_id pcprox_table[] = {
-    { USB_DEVICE(0x1234, 0x5678) }, // Replace with your vendor and device IDs
-    {}
-};
+ * pcprox_module.c - The simplest kernel module. 
 
-MODULE_DEVICE_TABLE(usb, pcprox_table);
+ */ 
 
-static int pcprox_probe(struct usb_interface *interface, const struct usb_device_id *id)
-{
-    // Probe function, called when device is connected
-    printk(KERN_INFO "PCProx device connected\n");
-    return 0; // Success
-}
+#include <linux/module.h> /* Needed by all modules */ 
 
-static void pcprox_disconnect(struct usb_interface *interface)
-{
-    // Disconnect function, called when device is disconnected
-    printk(KERN_INFO "PCProx device disconnected\n");
-}
+#include <linux/printk.h> /* Needed for pr_info() */ 
 
-static struct usb_driver pcprox_driver = {
-    .name = "pcprox",
-    .id_table = pcprox_table,
-    .probe = pcprox_probe,
-    .disconnect = pcprox_disconnect,
-};
+ 
 
-static int __init pcprox_init(void)
-{
-    // Module initialization
-    return usb_register(&pcprox_driver);
-}
+int init_module(void) 
 
-static void __exit pcprox_exit(void)
-{
-    // Module cleanup
-    usb_deregister(&pcprox_driver);
-}
+{ 
 
-module_init(pcprox_init);
-module_exit(pcprox_exit);
+    pr_info("Hello world 1.\n"); 
+
+ 
+
+    /* A non 0 return means init_module failed; module can't be loaded. */ 
+
+    return 0; 
+
+} 
+
+ 
+
+void cleanup_module(void) 
+
+{ 
+
+    pr_info("Goodbye world 1.\n"); 
+
+} 
+
+ 
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("PCProx USB RF Card Reader Module");
